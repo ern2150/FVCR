496f607f9bc72353e3ef494010320760e593862a
ern2150 <ern2150@users.noreply.github.com>
2021-06-04 14:24:01 -0500

    only latest file generates clips, clarifying

--- fingerprint-xml-hash.sh
+++ fingerprint-xml-hash.sh
@@ -1,42 +1,58 @@
-filename=$1
+vfile=$1
 
-ls $filename
+ls $vfile
 
-FINGERPRINT_XML=$filename.fingerprint.xml
-FINGERPRINT_TXT=$filename.fingerprint.log
-FINGERPRINT_CSV=$filename.fingerprint.csv
+FINGERPRINT_XML=$vfile.fingerprint.xml
+FINGERPRINT_TXT=$vfile.fingerprint.log
+FINGERPRINT_CSV=$vfile.fingerprint.csv
 
 
-if [ ! -e "$FINGERPRINT_XML" ]; then
-	ffmpeg -i $filename -vf signature=format=xml:filename="$FINGERPRINT_XML" -map 0:v -f null -
-fi
-
-ls $FINGERPRINT_XML
-
-if [ ! -e "$FINGERPRINT_TXT" ]; then
-	xmlstarlet sel -N "m=urn:mpeg:mpeg7:schema:2001" -t -m "m:Mpeg7/m:DescriptionUnit/m:Descriptor/m:VideoSignatureRegion/m:VSVideoSegment" -v m:StartFrameOfSegment -o ':' -v m:EndFrameOfSegment -o ':' -m m:BagOfWords -v "translate(.,' ','')" -o ':' -b -n "${FINGERPRINT_XML}" > "$FINGERPRINT_TXT";
-fi
-
-ls $FINGERPRINT_TXT
 
 if [ ! -e "$FINGERPRINT_CSV" ]; then
-	cat $FINGERPRINT_TXT | cut -d ':' -f3-9 | awk '{cmd="echo "$1" | md5";cmd | getline x;close(cmd);print x;}' > $filename.fingerprint.md5
+ if [ ! -e "$FINGERPRINT_TXT" ]; then
+  if [ ! -e "$FINGERPRINT_XML" ]; then
+	echo "obtaining XML signature by segment for video file"
+	ffmpeg -i $vfile -vf signature=format=xml:filename="$FINGERPRINT_XML" -map 0:v -f null -
+  fi
 
-	cat $FINGERPRINT_TXT | cut -d ':' -f1-2 > $filename.fingerprint.frames
+  echo "XML signature for video file:"
+  ls -l $FINGERPRINT_XML
 
-	paste $filename.fingerprint.frames $filename.fingerprint.md5 | tr ':' '\t' > $FINGERPRINT_CSV
+	echo "flattening XML signature"
+	xmlstarlet sel -N "m=urn:mpeg:mpeg7:schema:2001" -t -m "m:Mpeg7/m:DescriptionUnit/m:Descriptor/m:VideoSignatureRegion/m:VSVideoSegment" -v m:StartFrameOfSegment -o ':' -v m:EndFrameOfSegment -o ':' -m m:BagOfWords -v "translate(.,' ','')" -o ':' -b -n "${FINGERPRINT_XML}" > "$FINGERPRINT_TXT";
+ fi
 
-	rm $filename.fingerprint.md5
-	rm $filename.fingerprint.frames
+ echo "flattened XML signature:"
+ ls -l $FINGERPRINT_TXT
+
+	#cat $FINGERPRINT_TXT | cut -d ':' -f3-9 | awk '{cmd="echo "$1" | md5";cmd | getline x;close(cmd);print x;}' > $vfile.fingerprint.md5
+
+	#cat $FINGERPRINT_TXT | cut -d ':' -f1-2 > $vfile.fingerprint.frames
+
+	#paste $vfile.fingerprint.frames $vfile.fingerprint.md5 | tr ':' '\t' > $FINGERPRINT_CSV
+
+	#rm $vfile.fingerprint.md5
+	#rm $vfile.fingerprint.frames
+
+	echo "compressing flat signature to spreadsheet"
+	cat $FINGERPRINT_TXT | awk -F':' '{cmd="echo "$1" | md5";cmd | getline x;close(cmd);print $1 "\t" $2 "\t" x;}' > $FINGERPRINT_CSV
 fi
 
-ls $FINGERPRINT_CSV
+echo "compressed signature spreadsheet:"
+ls -l$FINGERPRINT_CSV
 
 
-if [ ! -e "$filename.ffmpeg.sh" ]; then
-	grep "\t" *.fingerprint.csv | tr ':' ' ' | sort -k4 | awk '{print $2 " " $3 " " $1 ":" $4}' | uniq -c -f2 | tr ':' ' ' | uniq -c -f4 | sort -rh | tr -s ' ' | grep "^[[:space:]]2" | awk '{print "ffmpeg -ss " $3/30 " -i " substr($5,0,index($5,".fingerprint")-1) " -to " $4/30-$3/30 " -n -vcodec copy -acodec copy " $6 "_" substr($5,0,index($5,".fingerprint")-1) }' > $filename.ffmpeg.sh
+if [ ! -e "$vfile.ffmpeg.sh" ]; then
+	echo "matching newest spreadsheet to the rest to build clip-generator script"	
+	grep "\t" *.fingerprint.csv | tr ':' ' ' | awk -v fpfx="$vfile" '{print $2 " " $3 " " index($1,fpfx) "_" $1 " " $4}' | sort -r -k4 -k3 | uniq -c -f2 | tr ':' ' ' | uniq -c -f4 | sort -rh | tr -s ' ' | grep "^[[:space:]]2.*1_" | awk '{print "ffmpeg -ss " $3/30 " -i " substr($5,3,index($5,".fingerprint")-3) " -to " $4/30-$3/30 " -n -vcodec copy -acodec copy " $6 "_" substr($5,3,index($5,".fingerprint")-3) }' > $vfile.ffmpeg.sh 
+#grep "\t" *.fingerprint.csv | tr ':' ' ' | sort -k4 | awk '{print $2 " " $3 " " $1 ":" $4}' | uniq -c -f2 | tr ':' ' ' | uniq -c -f4 | sort -rh | tr -s ' ' | grep "^[[:space:]]2" | awk '{print "ffmpeg -ss " $3/30 " -i " substr($5,0,index($5,".fingerprint")-1) " -to " $4/30-$3/30 " -n -vcodec copy -acodec copy " $6 "_" substr($5,0,index($5,".fingerprint")-1) }' > $filename.ffmpeg.sh
 fi
 
-ls $filename.ffmpeg.sh
-bash $filename.ffmpeg.sh
-rm $filename.ffmpeg.sh
+echo "clip generator script:"
+ls $vfile.ffmpeg.sh
+
+echo "generating clips"
+bash $vfile.ffmpeg.sh
+
+echo "cleaning up"
+rm $vfile.ffmpeg.sh
